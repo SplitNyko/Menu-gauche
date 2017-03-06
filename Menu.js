ec47faa550dcbc494810d0f851d76b344047d2f8
Nicolas MAROLLE <nicolas.marolle@viacesi.fr>
2017-03-07 00:33:57 +0100

    Menu gauche first Comit
    Ajout des fichiers du menu gauche

---
+++ Menu.js
@@ -0,0 +1,34 @@
+$(function () {
+    $('.menuLeftMenu .menuLeftIcon').click(function(e) {
+        e.preventDefault();
+        $('body').toggleClass('open');
+    });
+
+    $('.menuLeftIcons .menuLeftIcon:nth-child(1)').click(function() {
+        $('body').addClass('yellow');
+        $('body').removeClass('red');
+        $('body').removeClass('blue');
+        $('body').removeClass('brown');
+    });
+    $('.menuLeftIcons .menuLeftIcon:nth-child(2)').click(function(e) {
+        e.preventDefault();
+        $('body').addClass('red');
+        $('body').removeClass('yellow');
+        $('body').removeClass('blue');
+        $('body').removeClass('brown');
+    });
+    $('.menuLeftIcons .menuLeftIcon:nth-child(3)').click(function(e) {
+        e.preventDefault();
+        $('body').addClass('blue');
+        $('body').removeClass('red');
+        $('body').removeClass('yellow');
+        $('body').removeClass('brown');
+    });
+    $('.menuLeftIcons .menuLeftIcon:nth-child(4)').click(function(e) {
+        e.preventDefault();
+        $('body').addClass('brown');
+        $('body').removeClass('red');
+        $('body').removeClass('yellow');
+        $('body').removeClass('blue');
+    });
+});
